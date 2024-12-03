f461724f476a741431bb5d2a497db5aec71b6542
lupeichun <holmesflying222@gmail.com>
2024-12-03 19:53:52 +0800

    feat:設置主視窗 Bar 選項

---
+++ Global.cs
@@ -0,0 +1,15 @@
+public class Global
+{
+    /*
+        Gameboy Screen Size
+    */
+    public static int SCREEN_WIDTH = 160;
+    public static int SCREEN_HEIGHT = 144;
+    public static int SCREEN_SCALE = 6; // 縮放比例
+
+
+    /*
+        Gameboy Settings
+    */
+    public static double GAME_SPEED = 1.0; // 遊戲速度
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,130 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Cairo;
+using Gtk;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        ShowWindow();
+    }
+
+    public static void ShowWindow()
+    {
+        Application.Init();
+
+        // 視窗
+        Window window = new Window("Gameboy 模擬器");
+        window.SetDefaultSize(Global.SCREEN_WIDTH * Global.SCREEN_SCALE, Global.SCREEN_HEIGHT * Global.SCREEN_SCALE);
+        window.DeleteEvent += (o, e) => Application.Quit();
+        window.SetPosition(WindowPosition.Center);
+
+        // 垂直容器
+        VBox vBox = new VBox(false, 2);
+
+        // 上方選單列
+        MenuBar menuBar = new MenuBar();
+
+        /*
+            選項一：
+                檔案
+                    |_ 開啟檔案
+        */
+        MenuItem fileMenuItem = new MenuItem("檔案");
+        Menu fileMenuItemSub = new Menu();
+        fileMenuItem.Submenu = fileMenuItemSub;
+
+        // 開啟檔案
+        MenuItem openFileMenuItem = new MenuItem("開啟檔案");
+        openFileMenuItem.Activated += (sender, e) =>
+        {
+            FileChooserDialog fileChooser = new FileChooserDialog(
+                "選擇 gb 檔案",
+                window,
+                FileChooserAction.Open,
+                "取消", ResponseType.Cancel,
+                "開啟", ResponseType.Accept
+            );
+
+            // 只顯示 .gb 檔案
+            FileFilter fileFilter = new FileFilter();
+            fileFilter.Name = "Gameboy ROM Files (*.gb)";
+            fileFilter.AddPattern("*.gb");
+            fileChooser.Filter = fileFilter;
+
+            // 選擇完檔案
+            if (fileChooser.Run() == (int) ResponseType.Accept)
+            {
+                // 建立遊戲區塊
+                DrawingArea drawingArea = new DrawingArea();
+                drawingArea.SetSizeRequest(Global.SCREEN_WIDTH * Global.SCREEN_SCALE, Global.SCREEN_HEIGHT * Global.SCREEN_SCALE);
+                drawingArea.CanFocus = true;
+                drawingArea.FocusOnClick = true;
+                drawingArea.GrabFocus();
+
+                // 綁定 Keyboard 事件
+
+                vBox.PackStart(drawingArea, true, true, 0);
+                drawingArea.Show();
+            }
+
+            fileChooser.Destroy();
+        };
+
+        fileMenuItemSub.Append(openFileMenuItem);
+
+        // 選項二：遊戲速度
+        MenuItem speedMenuItem = new MenuItem("遊戲速度");
+        Menu speedMenuItemSub = new Menu();
+        speedMenuItem.Submenu = speedMenuItemSub;
+
+        // 遊戲速度選項
+        AddSpeedOption(speedMenuItemSub, "0.25", 0.25);
+        AddSpeedOption(speedMenuItemSub, "0.5", 0.5);
+        AddSpeedOption(speedMenuItemSub, "0.75", 0.75);
+        AddSpeedOption(speedMenuItemSub, "1", 1.0, true); // 預設為 1
+        AddSpeedOption(speedMenuItemSub, "1.25", 1.25);
+        AddSpeedOption(speedMenuItemSub, "1.5", 1.5);
+        AddSpeedOption(speedMenuItemSub, "1.75", 1.75);
+        AddSpeedOption(speedMenuItemSub, "2", 2.0);
+        
+        
+        // 添加到 Menubar
+        menuBar.Append(fileMenuItem);
+        menuBar.Append(speedMenuItem);
+
+
+        // 添加到 vbox
+        vBox.PackStart(menuBar, false, false, 0);
+
+        // 添加到 window
+        window.Add(vBox);
+
+        window.ShowAll();
+        Application.Run();
+    }
+
+    private static void AddSpeedOption(Menu speedMenu, string label, double value, bool isDefault = false)
+    {
+        MenuItem menuItem = new MenuItem(isDefault ? $"✔ {label}" : label);
+
+        menuItem.Activated += (sender, e) =>
+        {
+            // 更新所有選單項的標籤
+            foreach (MenuItem item in speedMenu)
+            {
+                item.Label = item.Label.Replace("✔ ", "");
+            }
+
+            // 為當前選項添加 "V "
+            menuItem.Label = $"✔ {label}";
+
+            // 更新遊戲速度
+            Global.GAME_SPEED = value;
+
+            // 在終端輸出當前選擇的速度
+            Console.WriteLine($"遊戲速度已設置為 {value}x");
+        };
+
+        speedMenu.Append(menuItem);
+    }
+}
